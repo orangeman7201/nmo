e35d118901f1fe94202c47309a0a0aa3c4f2f86d
Orangeman7201 <aki.jazz1027@gmail.com>
2024-03-02 18:51:48 +0900

    関連を作成

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,6 @@
 class Condition < ApplicationRecord
   validates :detail, presence: true
   validates :occurred_date, presence: true
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_131145) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_094632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_24_131145) do
     t.date "occurred_date", null: false, comment: "症状があった日"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_conditions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_24_131145) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "conditions", "users"
 end
